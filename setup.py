018b81b2aed1ac98806b2c6a011f4c9969bd6003
Sebastiano Milardo <s.milardo@hotmail.it>
2019-08-28 11:42:43 -0400

    Testing on test.pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from distutils.core import setup
 import setuptools
 
-classifiers = ['Development Status :: 3 - Alpha',
+
+setup(name='pychoropleth',
+    version='0.2.2',
+    description='A simple library to create choropleth maps from geopandas dataframes using mplleaflet',
+    packages=setuptools.find_packages(),
+    author='Sebastiano Milardo',
+    author_email='milardo@mit.edu',
+    license = 'MIT',
+    install_requires = ['mplleaflet','matplotlib','pandas','numpy','geopandas','shapely','osmnx','descartes'],
+    url = 'https://github.com/SebMilardo/pychoropleth',
+    project_urls={
+        'source': 'https://github.com/SebMilardo/pychoropleth/',
+    },
+    zip_safe=False,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers = ['Development Status :: 3 - Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'Intended Audience :: Science/Research',
@@ -11,17 +27,4 @@ classifiers = ['Development Status :: 3 - Alpha',
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Programming Language :: Python :: 3']
-
-
-setup(name='pychoropleth',
-    version='0.2',
-    description='A simple library to create choropleth maps from geopandas dataframes using mplleaflet',
-    packages=['pychoropleth',],
-    author='Sebastiano Milardo',
-    author_email='milardo@mit.edu',
-    license = 'MIT',
-    install_requires = ['mplleaflet','matplotlib','pandas','numpy','geopandas','shapely','osmnx','descartes'],
-    url = 'https://github.com/SebMilardo/pychoropleth',
-    zip_safe=False,
-    long_description=open('README.md').read()
     )
