e3970fcb91245f51c0291e6bb9a1b9d6c93b2875
Sebastiano Milardo <s.milardo@hotmail.it>
2019-08-27 21:41:59 -0400

    Version 0.2

--- pychoropleth/__init__.py
+++ pychoropleth/__init__.py
@@ -1,113 +1,91 @@
 import mplleaflet
 from matplotlib import pyplot as plt
 import numpy as np
-import geopandas as gpd
-from shapely.geometry import Point, Polygon, MultiPolygon
+import geopandas
+from shapely.geometry import Point, Polygon
 import osmnx as ox
-from geopy import distance
-from geopy import Point as gPoint
 from descartes import PolygonPatch
-from openpyxl.utils.cell import get_column_letter, column_index_from_string, coordinate_from_string
+import random
+import pandas as pd
 
 
-def to_coordinates(i,j):
-    return get_column_letter(i+1) + str(int(j))
-
-
-def from_coordinates(string):
-    i,j = coordinate_from_string(string)
-    return column_index_from_string(i)-1,j 
-
-
-def recover_id(start, cell, cell_size):
-    minx, miny, maxx, maxy = cell.bounds
-    idx = (minx - start[0]) / cell_size
-    idy = (miny - start[1]) / cell_size
-    return idx, idy
-
-
-def get_l(cell):
-    minx, miny, maxx, maxy = cell.bounds
-    return maxx - minx
-
-
-def df2geo(df, lat="latitude", lon="longitude", crs=None):
+def df_to_gdf(df, 
+              latitude="latitude", 
+              longitude="longitude", 
+              crs=None):
+    if type(df) is geopandas.GeoDataFrame:
+        return df
+    
     if crs is None:
         crs = {'init': 'epsg:4326'}
-    geo = gpd.GeoDataFrame(df)
-    geo["geometry"] = [Point(x[lon], x[lat]) for _, x in geo.iterrows()]
-    geo.crs = crs
-    return geo
+    gdf = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df[longitude], df[latitude]))
+    gdf.crs = crs
+    return gdf
 
 
-def get_grid_points(points, lat_long_bounds, grid_size=50):
-    points = ox.project_gdf(points)
-    crs = points.crs
-    box = ox.project_geometry(Polygon(lat_long_bounds))[0]
+def _coordinate_to_id(coordinate, bound, grid_size):
+    return int(np.floor(float(coordinate - bound) / grid_size))
+
+
+def _geometry_to_id(geometry, bounds, grid_size, max_i, max_j):
+    idx = _coordinate_to_id(geometry.x, bounds[0], grid_size)
+    idy = _coordinate_to_id(geometry.y, bounds[1], grid_size)
+    if idx >= 0 and idy >= 0 and idx <= max_i and idy <= max_j:
+        return(idx,idy)
+    else:
+        return np.nan
+
+    
+def _add_cell_id(gdf, grid_size, bounds=None):
+    if bounds is None:
+        maxx = gdf.geometry.dropna().x.max()
+        minx = gdf.geometry.dropna().x.min()
+        maxy = gdf.geometry.dropna().y.max()
+        miny = gdf.geometry.dropna().y.min()
+        bounds = [(minx,miny),(maxx,miny),(maxx,maxy),(minx,maxy),(minx,miny)]
+        
+    gdf = ox.project_gdf(gdf)
+    crs = gdf.crs
+    box = ox.project_geometry(Polygon(bounds))[0]
     bounds = box.bounds
-    squares = []
 
-    for idi, i in enumerate(range(int(bounds[0]),int(bounds[2]), grid_size)):
-        for idj, j in enumerate(range(int(bounds[1]),int(bounds[3]), grid_size)):
-            squares.append([to_coordinates(idi,idj),
-                            Polygon([[i,j],[i+grid_size,j],[i+grid_size,j+grid_size], [i,j+grid_size]])])
-
-    max_i = idi + 1
-    max_j = idj + 1
-
-    grid = [[coord, square] for coord, square in squares]
-
-    indexes = np.array(grid)[:,0].tolist()
-    grid_all = MultiPolygon(np.array(grid)[:,1].tolist())
-    grid = gpd.GeoDataFrame(grid_all, columns=["geometry"], crs=crs)
-    grid["cell_id"] = indexes
-    cell_set = set(indexes)
+    max_i = _coordinate_to_id(bounds[2], bounds[0], grid_size)
+    max_j = _coordinate_to_id(bounds[3], bounds[1], grid_size)
 
     cell_per_point = []
-    for _, row in points.iterrows():
-        idx = int(np.ceil(float(row.geometry.x - bounds[0]) / grid_size))
-        idy = int(np.ceil(float(row.geometry.y - bounds[1]) / grid_size))
-        if idx >= 0 and idy >= 0 and idx <= max_i and idy <= max_j:
-            coordinate = to_coordinates(idx,idy)
-            if coordinate in cell_set:
-                cell_per_point.append(coordinate)
-            else:
-                cell_per_point.append(np.nan)
-        else:
-            cell_per_point.append(np.nan)
-    points["cell_id"] = cell_per_point
-    grid = ox.project_gdf(grid, to_latlong=True)
-    return grid, points
+    gdf["cell_id"] = gdf.geometry.apply(lambda x: _geometry_to_id(x, bounds, grid_size, max_i, max_j))
+    return bounds, gdf
 
 
-def plot_choropleth(grid, points, intersection=None, tiles='cartodb_positron', vmax=10, cmap="viridis", 
-                    column="latitude"):
-    if intersection is not None:
-        grid = gpd.GeoDataFrame([x for x in grid.values if intersection.intersects(x[0])])
-    grid.crs = {'init': 'epsg:4326'}
-    grid.columns = ["geometry","cell_id"]
+def _cell_id_to_polygon(cell_id, bounds, grid_size):
+    i = bounds[0] + (cell_id[0]*grid_size)
+    j = bounds[1] + (cell_id[1]*grid_size)
+    return Polygon([[i,j],[i+grid_size,j],[i+grid_size,j+grid_size], [i,j+grid_size]])
 
-    count_cell = points[["cell_id",column]].dropna().groupby("cell_id").count()
-    count_cell.columns = ["no"]
+
+def choropleth(df, grid_size,
+               bounds=None, 
+               latitude="latitude", 
+               longitude="longitude", 
+               tiles='cartodb_positron', 
+               vmax=10, 
+               cmap="viridis", 
+               column="latitude", 
+               crs=None,
+               figsize=None):
+    if figsize is None:
+      figsize = [10,10] 
+    fig = plt.figure(figsize=figsize)
+
+    gdf = df_to_gdf(df, latitude, longitude, crs)
+    bounds, gdf = _add_cell_id(gdf, grid_size, bounds)
     
-    aggregated = count_cell.join(grid.set_index("cell_id"))
-    aggregated["color"] = aggregated.no.apply(lambda x: min(x,vmax)/vmax * 255)
-    
-    
-    fig = plt.figure(figsize=[10,10])
-    for idx, r in aggregated.iterrows():
-        color = plt.get_cmap(cmap).colors[int(r.color) if not np.isnan(r.color) else 0]
-        plt.gca().add_patch(PolygonPatch(r.geometry, alpha=0.5,color=color, zorder=2))
+    aggregated = gdf.groupby("cell_id").count()
+    if len(aggregated) > 0:
+      aggregated["geometry"] = aggregated.apply(lambda x: _cell_id_to_polygon(x.name, bounds, grid_size),axis=1)
+      aggregated["color"] = aggregated[column].apply(lambda x: min(x,vmax)/vmax * 255)
+      aggregated = ox.project_gdf(geopandas.GeoDataFrame(aggregated, crs=gdf.crs),to_latlong=True)
+      for idx, r in aggregated.iterrows():
+          color = plt.get_cmap(cmap).colors[int(r.color) if not np.isnan(r.color) else 0]
+          plt.gca().add_patch(PolygonPatch(r.geometry, alpha=0.5,color=color, zorder=2))
     return mplleaflet.display(fig, tiles=tiles)
-
-
-def choropleth(geodf, bounds=None, intersection=None, lat="latitude", lon="longitude", 
-               grid_size=50, tiles='cartodb_positron', vmax=10, cmap="viridis", column="latitude"):
-    if bounds is None:
-        maxx = geodf[lon].dropna().max()
-        minx = geodf[lon].dropna().min()
-        maxy = geodf[lat].dropna().max()
-        miny = geodf[lat].dropna().min()
-        bounds = [(minx,miny),(maxx,miny),(maxx,maxy),(minx,maxy),(minx,miny)]
-    grid, points = get_grid_points(geodf, bounds, grid_size=grid_size)
-    return plot_choropleth(grid, points, intersection=intersection, tiles=tiles, vmax=vmax, cmap=cmap, column=column)
--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ classifiers = ['Development Status :: 3 - Alpha',
 
 
 setup(name='pychoropleth',
-    version='0.1',
+    version='0.2',
     description='A simple library to create choropleth maps from geopandas dataframes using mplleaflet',
     author='Sebastiano Milardo',
     author_email='milardo@mit.edu',
     license = 'MIT',
-    install_requires = ['mplleaflet','matplotlib','pandas','numpy','geopandas','shapely','osmnx','geopy','descartes','openpyxl'],
+    install_requires = ['mplleaflet','matplotlib','pandas','numpy','geopandas','shapely','osmnx','descartes'],
     url = 'https://github.com/SebMilardo/pychoropleth',
     zip_safe=False
     )
