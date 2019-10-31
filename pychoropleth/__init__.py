d372c11ddf95f0f77854bfff216aa4f15cecd9c1
Sebastiano Milardo <s.milardo@hotmail.it>
2019-10-31 11:51:11 -0400

    Add cell_id as standalone function

--- pychoropleth/__init__.py
+++ pychoropleth/__init__.py
@@ -63,6 +63,12 @@ def _cell_id_to_polygon(cell_id, bounds, grid_size):
     return Polygon([[i,j],[i+grid_size,j],[i+grid_size,j+grid_size], [i,j+grid_size]])
 
 
+def add_cell_id(df, grid_size, bounds=None):
+    gdf = df_to_gdf(df, latitude, longitude, crs)
+    bounds, gdf = _add_cell_id(gdf, grid_size, bounds)
+    return bounds, gdf
+
+
 def create_grid(df, grid_size, bounds=None, latitude="latitude", longitude="longitude", column=None, crs=None, 
                 vmax=10):
     gdf = df_to_gdf(df, latitude, longitude, crs)
